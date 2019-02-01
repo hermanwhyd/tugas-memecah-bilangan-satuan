c1b8cc8ff440770743a631843170a9691acc5e9c
HermanW <hwahyudi@xl.co.id>
2019-02-01 10:00:47 +0700

    first commit

---
+++ src/aplikasi/pecah/satuan/AplikasiPecahSatuan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.pecah.satuan;
+
+/**
+ *
+ * @author HWAHYUDI
+ */
+public class AplikasiPecahSatuan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        mainForm.setLocationRelativeTo(null);
+        mainForm.setVisible(true);
+    }
+    
+}
